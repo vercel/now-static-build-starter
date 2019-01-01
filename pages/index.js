7a58059fd21e60529f22de2529c93ab6a7634c69
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2019-01-01 05:50:03 +0530

    Change the welcome text (#1)

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => (
   <div>
-    Hello World
+    Hello World from Now Static Build
     <br/><br/>
     Visit the <Link href="/contact"><a>Contact Us</a></Link> page.
   </div>
